1af9438d624390afd56dd06b0e50b70d46126911
Crea7Net <l.cote@codeur.online>
2022-06-28 11:33:10 +0200

    Restructuration des fichiers du projet AWW

---
+++ partials/header.php
@@ -0,0 +1,7 @@
+<header>
+
+<h2>AnyWhereWeb</h2>
+
+// include nav
+
+</header>
---
+++ partials/nav.php
@@ -0,0 +1,6 @@
+<nav>
+  <li><a href="#">Accueil</a></li>
+  <li><a href="realisations.html">Nos réalisations</a></li>
+  <li><a href="#">L'Équipe</a></li>
+  <li><a href="#">Contactez-nous !</a></li>
+</nav>
