2c863c2f7a0caf3613ce039cc7ab6903c3d6983c
Crea7Net <l.cote@codeur.online>
2022-06-29 16:23:13 +0200

    Add Services

---
+++ page/accueil/services.php
@@ -0,0 +1,26 @@
+<section class="lagence" id="agence">
+
+  <h3 class="title">Nos services à votre service</h3>
+
+  <p class="paragraph">Réalisation de sites internet, applications et stratégie digitale.</p>
+
+  <div class="paragraph">Profitez de l’expertise d’une agence web pour vous accompagner dans votre stratégie digitale. Nos experts sont là pour vous conseiller et vous guider sur les canaux digitaux les plus adaptés à votre business.</div>
+
+  <ul>
+    <li>Site e-commerce</li>
+    <li>application mobile</li>
+    <li>site vitrine</li>
+    <li>stratégie SEO ou social media</li>
+  </ul><br>
+  
+  Quelque soit votre besoin, AnyWhereWeb vous permet de toucher votre cible avec efficacité et de booster votre chiffre d’affaires.
+
+    <div class="paragraph">Nous sommes spécialistes du sur-mesure, contactez notre agence web pour obtenir un devis gratuit et personnalisé</div>
+
+  <hr>
+
+  <!-- 2do trouver nouvelle image -->
+  <ul class="grid">
+    <li class="small" style="background-image:url(../../assets/image/desktop1.png) ;"></li>
+  </ul>
+</section>
